3ca217b2a2ccf7a86b222a56b1fdbab690addaee
Suhana Kumar <skumar50@stuy.edu>
2024-09-17 19:57:34 -0400

    K05

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,2 +1,20 @@
-f = open("krewes.txt", "r")
-print(f.read())
+import re
+import random
+file = open("krewes.txt", "r")
+line = file.read()
+g = line.split("@@@")
+dict1 = {"pd": [], "devo": [], "ducky": []}
+for i in range(len(g)):
+    lines = g[i]
+    f = lines.split("$$$")
+    dict1["pd"].append(f[0])
+    dict1["devo"].append(f[1])
+    dict1["ducky"].append(f[2])
+num = random.randint(0,50)
+
+    
+
+
+print(dict1["pd"][num] + " " + dict1["devo"][num] + " " + dict1["ducky"][num])
+
+    
