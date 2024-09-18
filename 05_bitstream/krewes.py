78c513e6eb3a00d280f8c82c3c361d54af0df45e
Suhana Kumar <skumar50@stuy.edu>
2024-09-18 11:42:06 -0400

    matching code to partners

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -9,14 +9,17 @@ import random
 with open("krewes.txt", "r") as file:
     f = file.read()
 tuples = f.split("@@@")
+#splits by devo
 devos = []
-for tuple in tuples:
+for tuple in tuples: #iterates through each tuple
     info = tuple.split("$$$")
+#splits each devo's information into thier period, name and duck name
     if len(info) == 3:
         period, devo, ducky = info
-        devos.append({'period': period, 'devo': devo, 'ducky': ducky})
+        devos.append({'period': period, 'devo': devo, 'ducky': ducky})#adds the info for each devo into a list as its own index
 if devos:
-    random_devo = random.choice(devos)
+    random_devo = random.choice(devos)#chosses random devo
     print(f"Devo: {random_devo['devo']}, Period: {random_devo['period']}, Ducky: {random_devo['ducky']}")
 else:
         print("No devos found.")
+        
