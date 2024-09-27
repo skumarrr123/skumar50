aa4d4d1b5d4dae93195b658d982a78bee5d93ff2
Suhana Kumar <skumar50@stuy.edu>
2024-09-27 11:45:35 -0400

    added small thing

--- 11_flask-static/app.py
+++ 11_flask-static/app.py
@@ -1,6 +1,10 @@
-# Clyde 'Thluffy' Sinclair
+# Suhana Kumar
+# K^3 (Suhana Kumar, Vedant Kothari, Kyle Lee)
 # SoftDev
-# Sep 2024
+# K11 basics of static folder
+# <20244><09><26>
+# time spent: 0.5
+
 
 # DEMO
 # basics of /static folder
@@ -21,8 +25,13 @@ def h():
     print(__name__)
     return str(random.random())
 
+@app.route("/static/fixie.html")
+def g():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "TNPG: K^3 Names: Vedant Kothari, Suhana Kumar, Kyle Lee"
+
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
-
