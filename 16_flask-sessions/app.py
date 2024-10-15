260825a94a4075f8147c3caabb7366a596bbce39
Suhana Kumar <skumar50@stuy.edu>
2024-10-15 11:31:47 -0400

    fixed 16

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -5,31 +5,43 @@ K16 - Understanding cookies and log in sessions
 Time spent: 2
 2024-10-11
 '''
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-
-T_name = "K^3"
-roster = ["Suhana Kumar, Kyle Lee, Vedant Kothari"]
+app.secret_key = 'conortoglory'
+T_name = "Albania"
+roster = ["Suhana Kumar, Vedant Kothari, Kyle Lee"]
 
 @app.route('/')
 def primary():
-    return render_template('primary.html',T_name=T_name,roster=roster)
+    if 'username' in session:
+        return redirect(url_for('submit'))
+    return render_template('submit.html',T_name=T_name,roster=roster)
 
-@app.route('/submit', methods=['GET', 'POST'])
+@app.route('/submit', methods=['POST'])
 def submit():
-    if request.method == 'POST'
-        username = request.form('username')
-    if request.method == 'GET'
-        username = request.args.get('username')
-        
-    method_used = request.method
-    greeting = "Hey there "+username+". We hope you like this page"
-    explanation = '''
-    GET sends data via the URL, making it more useful for taking information.
-    POST sends data through the body which makes it more useful for modifying data on the server side.
-    '''
-    return render_template('secondary.html', username=username, method=method_used, greeting=greeting, explanation=explanation, T_name=T_name,roster=roster)
+    username = request.form.get('username')
+    session['username'] = username
+    return redirect(url_for('welcome'))
+
+@app.route('/welcome')
+def welcome():
+    if 'username' in session:
+        username = session['username']
+        greeting = "Hey there "+username+". We hope you like this page"
+        method_used = request.method
+        explanation = '''
+        This is a flask app which checks if you login via a correct username. You will be logged in until you click logout. The app stores sessions via cookies, and the session cookie is sent to your browser.
+        '''
+        return render_template('response.html', username=username, method=method_used, greeting=greeting, explanation=explanation, T_name=T_name,roster=roster)
+    else:
+        return redirect(url_for('primary'))
+
+@app.route('/unsubmit')
+def unsubmit():
+    username = session.pop('username', None)
+    return render_template('unsubmit.html', T_name=T_name, roster=roster)
 
 if __name__ == '__main__':
     app.run(debug=True)
